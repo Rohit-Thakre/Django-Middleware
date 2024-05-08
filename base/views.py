00d0c64a224088e7b52cccf05dc74f01dc2fea85
Rohit Thakre <thake785@gmail.com>
2024-05-08 22:37:30 +0530

    project basic settings added.

--- base/views.py
+++ base/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+class Home(APIView):
+    permission_classes = [IsAuthenticated]
+    def initial(self, request, *args, **kwargs):
+        return super().initial(request, *args, **kwargs)
+    
+    def get(self, request, *args, **kwargs):
+        return Response({"message": "get response"},status=status.HTTP_200_OK)
+    
+    def post(self, request, *args, **kwargs):
+        return Response({"message": "post response"},status=status.HTTP_201_CREATED)
+    
